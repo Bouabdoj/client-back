cdf7937324e74f7045d9dc692fd568426e7af27d
Jacques Bou Abdo <89325077+Bouabdoj@users.noreply.github.com>
2021-09-04 12:37:13 -0500

    Add files via upload

--- eventPage.js
+++ eventPage.js
@@ -3,17 +3,38 @@ var contextMenuItem = {
     "title": "Add Domain",
     "contexts":["link"]
 };
-chrome.contextMenus.create(contextMenuItem);
 
+//In Chrome you should create the context menu just once after install/update.
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create(contextMenuItem);
+});
+
+//OR
+
+//Alternatively, you can simply suppress the error by accessing lastError in the callback:
+/*
+chrome.contextMenus.create(contextMenuItem, 
+            () => chrome.runtime.lastError);
+
+*/
+
+/*
+When you right click on a link, it will add the link (after removing www.) 
+in a storage with an id = "newdomain"
+*/
 chrome.contextMenus.onClicked.addListener(function(clickData){
     if(clickData.menuItemId =="AddDomain123" )
     {
         var url = clickData.linkUrl;
         Add_domain(url);
-
     }
 });
 
+
+/*
+Listening if the storage that contains the domain name changes a notification will popup 
+and the badge will be updated to the new domain name 
+*/
 chrome.storage.onChanged.addListener(function(changes, storagename){
     var newval ;
     var notifOptions ={
@@ -37,6 +58,9 @@ chrome.storage.onChanged.addListener(function(changes, storagename){
 
 })
 
+/* 
+Function that will add the domain in the storage with id = "newdomain"
+*/
 function Add_domain(url) {
     let domain = (new URL(url));
     domain = domain.hostname.replace('www.','');
@@ -44,7 +68,10 @@ function Add_domain(url) {
     chrome.storage.sync.set({'newdomain': domain});
 };
 
-
+/*
+it listens for messages coming from content.js and add the domain in 
+the storage but in our case content.js won't send messages
+*/
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.todo == "addDomain")
     {
@@ -52,18 +79,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
             let url = tabs[0].url;
             Add_domain(url);
         });
-        
     }
 });
 
+
 chrome.webNavigation.onBeforeNavigate.addListener(function(data){
     var profile = null;
-    if(data.url != "about:blank" && !data.url.includes("google"))
+    if(data.url != "about:blank" && !data.url.includes("google") && !data.url.includes("new-tab-page"))
     {
     var URL_formatted = (new URL(data.url));
     let domain_triggered = (new URL(data.url));
 
     domain_triggered = domain_triggered.hostname.replace('www.','');
+
     chrome.storage.sync.get('domains', function(domain){
         var domain_array=[];
         for(elements in domain.domains) 
@@ -79,33 +107,44 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(data){
             chrome.storage.sync.get(profile, function(cookies_list){
             
                 for(cookie in cookies_list)
-                {
-                    
-                    console.log(cookies_list[cookie].domain+" "+ cookies_list[cookie].storeId);
+                {     
+                    if(profile == cookie){
+                        for(i in cookies_list[cookie])
+                        {
+                            console.log(cookies_list[cookie][i].domain + " " + cookies_list[cookie][i].expirationDate);
+                            var domain = cookies_list[cookie][i].domain;
+                            var expirationDate = cookies_list[cookie][i].expirationDate;
+                            var hostOnly = cookies_list[cookie][i].hostOnly;
+                            var httpOnly = cookies_list[cookie][i].httpOnly;
+                            var name = cookies_list[cookie][i].name;
+                            var path = cookies_list[cookie][i].path;
+                            var sameSite = cookies_list[cookie][i].sameSite;
+                            var secure = cookies_list[cookie][i].secure;
+                            var url = cookies_list[cookie][i].url;
+                            var value = cookies_list[cookie][i].value;
+                            var storeId = cookies_list[cookie][i].storeId;
+                            var session = cookies_list[cookie][i].session;
 
-                    var domain = cookies_list[cookie].domain;
-                    var expirationDate = cookies_list[cookie].expirationDate;
-                    var hostOnly = cookies_list[cookie].hostOnly;
-                    var httpOnly = cookies_list[cookie].httpOnly;
-                    var name = cookies_list[cookie].name;
-                    var path = cookies_list[cookie].path;
-                    var sameSite = cookies_list[cookie].sameSite;
-                    var secure = cookies_list[cookie].secure;
-                    var url = cookies_list[cookie].url;
-                    var value = cookies_list[cookie].value;
+                            var domain_url = '';
+                            domain_url += secure ? 'https://' : 'http://';
+                            domain_url += domain.charAt(0) == '.' ? 'www' : '';
+                            domain_url += domain;
+                            domain_url += path;
 
-                    try
-                    {
-                    //chrome.cookies.remove()
-                    var updated_cookie = {'domain':domain ,'url':'https://www'+domain, 'expirationDate':expirationDate, 'httpOnly': httpOnly, 'name':name, 'path':path+profile+"/", 'sameSite':sameSite, 'secure':secure, 'value':value, 'storeId': 1};
-                    chrome.cookies.set(updated_cookie);
-                    console.log("Done");
-                    }
-                    catch
-                    {
-                        console.log("error");
-                    }
-                    
+                            try
+                            {
+                            //chrome.cookies.remove()
+                            var updated_cookie = {'domain':domain ,'expirationDate':expirationDate, 'url':domain_url, 'httpOnly': httpOnly, 'name': name, 'path':path, 'sameSite':sameSite, 'secure':secure,'storeId': storeId ,'value':value};
+                            
+                            chrome.cookies.set(updated_cookie);
+                            console.log("Done");
+                            }
+                            catch(error)
+                            {
+                                console.log(error);
+                            }
+                        }
+                    }                    
                 }
             });
             chrome.cookies.getAll({'url': data.url}, function(cookies_list){
@@ -128,7 +167,7 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(data){
 
 chrome.webNavigation.onCompleted.addListener(function(data){
     var profile = null;
-    if(data.url != "about:blank" && !data.url.includes("google"))
+    if(data.url != "about:blank" && !data.url.includes("google") && !data.url.includes("new-tab-page"))
     {
     var URL_formatted = (new URL(data.url));
     let domain_triggered = (new URL(data.url));
@@ -146,7 +185,6 @@ chrome.webNavigation.onCompleted.addListener(function(data){
         }
         if(profile)
         {
-
             chrome.cookies.getAllCookieStores(function(list){
                 for(cookiestore in list)
                 {
@@ -154,51 +192,138 @@ chrome.webNavigation.onCompleted.addListener(function(data){
                 }
             });
             var loaded_cookies=[];
-            //update this
-            //chrome.cookies.getAll({'path':'/'},function(cookies_list){
-            chrome.cookies.getAll({'url': data.url}, function(cookies_list){
+            var my_cookies=[];
+
+            /*
+            Remove the cookies from storage that are not related to the domain i'm opening (Ex: instagram)
+            and put them inside an array, because after that i'm going to remove all the cookies from the 
+            chrome.cookies.getAll and insert them inside the array and finally override the previous array 
+            of cookies stored in the storage.
+            */
+                   
+            chrome.cookies.getAll({'url':data.url}, function(cookies_list){
                 for(cookie in cookies_list)
                 {
-                    
+
                     console.log(cookies_list[cookie].domain+" "+ cookies_list[cookie].storeId);
 
                     var domain = cookies_list[cookie].domain;
                     var expirationDate = cookies_list[cookie].expirationDate;
                     var hostOnly = cookies_list[cookie].hostOnly;
-                    var httpOnly = cookies_list[cookie].httpOnly;
+                    var httpOnly =  cookies_list[cookie].httpOnly;
                     var name = cookies_list[cookie].name;
                     var path = cookies_list[cookie].path;
                     var sameSite = cookies_list[cookie].sameSite;
                     var secure = cookies_list[cookie].secure;
                     var url = cookies_list[cookie].url;
                     var value = cookies_list[cookie].value;
+                    var storeId = cookies_list[cookie].storeId;
 
-                    try
+                    try 
                     {
                         //update this
-                    //chrome.cookies.remove({'name':name,'url': url});
-                    chrome.cookies.remove({'name':name,'url': data.url});
-                    loaded_cookies.push({'domain':domain ,'url':'https://www'+domain, 'expirationDate':expirationDate, 'httpOnly': httpOnly, 'name':name, 'path':path+profile+"/", 'sameSite':sameSite, 'secure':secure, 'value':value, 'storeId': 1});
-                    
-                    //chrome.cookies.set(updated_cookie);
-                    console.log("Done");
+                        //chrome.cookies.remove({'name':name,'url': url});
+                        var domain_url = '';
+                        // get prefix, like https://www.
+                        domain_url += secure ? 'https://' : 'http://';
+                        domain_url += domain.charAt(0) == '.' ? 'www' : '';
+                        domain_url += domain;
+                        domain_url += path;
+                        chrome.cookies.remove({'name':name,'url': data.url});
+                        
+                        if(!secure)
+                        {
+                            domain = domain.charAt(0) == '.' ? domain.substr(1, domain.length) : domain;
+                        }
+                        
+                        loaded_cookies.push( {'domain':domain ,'expirationDate':expirationDate, 'url':domain_url, 'httpOnly': httpOnly, 'name': name, 'path':path, 'sameSite':sameSite, 'secure':secure, 'storeId': storeId ,'value':value});
+
+                        //chrome.cookies.set(updated_cookie);
+                        console.log("Done");                  
+                    } 
+                    catch (error) {
+                        console.log(error);    
                     }
-                    catch
-                    {
-                        console.log("error");
-                    }
-                 
-                    
                 }
-                chrome.storage.sync.set({profile:loaded_cookies})
             });
+            
+            chrome.storage.sync.get(profile, function(cookies_list){
+                for(cookie in cookies_list)
+                {
+                    if(profile == cookie)
+                    {
+                        for(i in cookies_list[cookie])
+                        {
+                            isFound = false;
+                            console.log(cookies_list[cookie][i].domain + " " + cookies_list[cookie][i].expirationDate);
+                            var domain = cookies_list[cookie][i].domain;
+                            var expirationDate = cookies_list[cookie][i].expirationDate;
+                            var hostOnly = cookies_list[cookie][i].hostOnly;
+                            var httpOnly = cookies_list[cookie][i].httpOnly;
+                            var name = cookies_list[cookie][i].name;
+                            var path = cookies_list[cookie][i].path;
+                            var sameSite = cookies_list[cookie][i].sameSite;
+                            var secure = cookies_list[cookie][i].secure;
+                            var url = cookies_list[cookie][i].url;
+                            var value = cookies_list[cookie][i].value;
+                            var storeId = cookies_list[cookie][i].storeId;
+                            var session = cookies_list[cookie][i].session;
+
+
+                            var domain_url = '';                            
+                            domain_url += secure ? 'https://' : 'http://';
+                            domain_url += domain.charAt(0) == '.' ? 'www' : '';
+                            domain_url += domain;
+                            domain_url += path;
+
+                            try
+                            {
+                                for(j in loaded_cookies)
+                                {
+                                    if(name.includes(loaded_cookies[j].name.charAt(0) == '.' ? loaded_cookies[j].name.substr(1, loaded_cookies[j].name.length) : loaded_cookies[j].name) && domain.includes(loaded_cookies[j].domain.charAt(0) == '.' ? loaded_cookies[j].domain.substr(1, loaded_cookies[j].domain.length) : loaded_cookies[j].domain))
+                                    {
+                                        domain_url += loaded_cookies[j].secure ? 'https://' : 'http://';
+                                        domain_url += loaded_cookies[j].domain.charAt(0) == '.' ? 'www' : '';
+                                        domain_url += loaded_cookies[j].domain;
+                                        domain_url += loaded_cookies[j].path;
+                                        isFound = true;
+                                        my_cookies.push( {'domain':loaded_cookies[j].domain ,'expirationDate':loaded_cookies[j].expirationDate, 'url':domain_url, 'httpOnly': loaded_cookies[j].httpOnly, 'name': loaded_cookies[j].name, 'path':loaded_cookies[j].path, 'sameSite':loaded_cookies[j].sameSite, 'secure':loaded_cookies[j].secure, 'storeId': loaded_cookies[j].storeId ,'value':loaded_cookies[j].value});
+                                        break;
+                                    }                                   
+                                }
+
+                                if(!isFound)
+                                {
+                                    my_cookies.push( {'domain':domain ,'expirationDate':expirationDate, 'url':domain_url, 'httpOnly': httpOnly, 'name': name, 'path':path, 'sameSite':sameSite, 'secure':secure, 'storeId': storeId ,'value':value});
+                                }
+                                
+                                console.log("Done");
+                            }
+                            catch(error)
+                            {
+                                console.log(error);
+                            }
+                        }
+                    }
+                }
+                if(my_cookies.length != 0)
+                {
+                    loaded_cookies = [];
+                    loaded_cookies = my_cookies;
+                }
+            });           
+
             chrome.cookies.getAll({'url': data.url}, function(cookies_list){
                 for(cookie in cookies_list)
                 {
                     console.log(cookies_list[cookie].domain+" "+ cookies_list[cookie].storeId+" "+ cookies_list[cookie].path);
                 }
+
+                if(loaded_cookies.length != 0)
+                    chrome.storage.sync.set({[profile]: []});
+                    chrome.storage.sync.set({[profile]: loaded_cookies});
+
             });
-            
 
         }
         else
@@ -209,5 +334,4 @@ chrome.webNavigation.onCompleted.addListener(function(data){
 
     });
 }
-
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
 $(function(){
-
+    /*
+        This function will retrieve from the storage the domain and will add it in the html tag with id="new_domain"
+        It will also retrieve all the compartments and add them in the select with id="selected_compartment_to_add_domain"
+    */
+   
     chrome.storage.sync.get('newdomain', function(newdomains){
         if(newdomains.newdomain)
         {
@@ -31,7 +35,18 @@ $(function(){
     
     chrome.storage.sync.get('Compartments', function(compartment){
         var compartment_array=[];
-        compartment_array.push({"value":$('#new_compartment').val()}); // the value of "Add Compartment" input (1)
+        var new_compartment_exists = false;
+        //The condition check if there is an empty input
+        //The for loop will check whether there is an existing compartment
+        if($('#new_compartment').val().length != 0){
+            for(elements in compartment.Compartments){
+                if($('#new_compartment').val() == compartment.Compartments[elements].value)
+                    new_compartment_exists = true;
+            }
+            if(!new_compartment_exists){    
+                compartment_array.push({"value":$('#new_compartment').val()}); // the value of "Add Compartment" input (1)
+            }
+        }
         for(elements in compartment.Compartments) 
         {
             compartment_array.push({"value":compartment.Compartments[elements].value});
@@ -50,7 +65,7 @@ $(function(){
             
             var select = document.getElementById("selected_compartment_to_add_profile"); // the dropbox of "Add Profile" (4)
             var options = select.options;
-         /*   for (var i=0; i<=select.length; i++) {
+        /*for (var i=0; i<=select.length; i++) {
                 select.options.remove(i);
             }*/ // I omitted
 
@@ -67,12 +82,21 @@ $(function(){
     $('#insert_profile').click(function(){ // when clicking the button "Add Profile" (5)
         chrome.storage.sync.get('profiles', function(profile){
             var profile_array=[];
-            var compartment_array=[];            
-            profile_array.push({'prof':$('#new_profile').val(), // the value of "Add Profile" input (3)
-                                'compartment': $('#selected_compartment_to_add_profile').find(":selected").text() 
-                                // the value of the selected item in the dropbox of "Add Profile" (4)
-                                });
-
+            var compartment_array=[];  
+            var new_profile_exists = false;
+            if($('#new_profile').val().length != 0){
+                for(elements in profile.profiles){
+                    if(profile.profiles[elements].prof == $('#new_profile').val() && profile.profiles[elements].compartment == $('#selected_compartment_to_add_profile').find(":selected").text())
+                        new_profile_exists = true;
+                }
+                if(!new_profile_exists){
+                    profile_array.push({'prof':$('#new_profile').val(), // the value of "Add Profile" input (3)
+                    'compartment': $('#selected_compartment_to_add_profile').find(":selected").text() 
+                    // the value of the selected item in the dropbox of "Add Profile" (4)
+                    });
+                }
+            }
+    
             for(elements in profile.profiles) 
             {
                 profile_array.push({'prof':profile.profiles[elements].prof, 
@@ -130,16 +154,29 @@ $(function(){
     //Create a new domain associated with the selected profile.
     $('#insert_domain').click(function(){
         chrome.storage.sync.get('domains', function(domain){
-            var domain_array=[];        
-            domain_array.push({
-                'domain':$('#new_domain').val(), 
-                'prof': $('#selected_profile_to_add_domain').find(":selected").text()});
+            var domain_array=[];
+            var isFound = false;     
 
+            var new_domain = $('#new_domain').val();
+            new_domain = new_domain.replace('https://','');
+            new_domain = new_domain.replace('http://','');
+            new_domain = new_domain.replace('www.','');
             for(elements in domain.domains) 
             {
                 //domain_array.push({'domain':domain.domains[elements].prof, 'prof':domain.domains[elements].compartment}); // i omitted it
                 domain_array.push({'domain':domain.domains[elements].domain, 'prof':domain.domains[elements].prof});
+                if(domain.domains[elements].domain == new_domain)
+                    isFound = true;
             }
+
+            if(!isFound)
+            {
+                domain_array.push({
+                    'domain':$('#new_domain').val(), 
+                    'prof': $('#selected_profile_to_add_domain').find(":selected").text()});    
+            }
+
+
         chrome.storage.sync.set({'domains': domain_array});
         chrome.storage.sync.set({'newdomain': null});
         $('#new_domain').val("");
@@ -195,7 +232,14 @@ $(function(){
             domain_array1 = domain.domains;
             
             var select = document.getElementById("selected_domain");
-
+            
+            /*
+                The next for is added to avoid duplicating data in the select because every time we click 
+                on the filter button the select will be filled with the domains again
+                So, this for will clear all domain in the select before re-filling it
+            */
+            select.options.length = 0;
+            
             for(index in domain_array1) {
                 select.options.add( new Option(domain_array1[index].domain, index));
             } 
